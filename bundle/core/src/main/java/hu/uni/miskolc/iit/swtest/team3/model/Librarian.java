5b0c463f772308d174b9d3b2298196b57fefc0b2
aronnagy <aron.nagy.1997@gmail.com>
2018-10-10 11:36:17 +0200

    Added extensions for classes Librarian & Reader

--- bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/team3/model/Librarian.java
+++ bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/team3/model/Librarian.java
@@ -1,4 +1,8 @@
 package hu.uni.miskolc.iit.swtest.team3.model;
 
-public class Librarian {
+public class Librarian extends User {
+
+    public Librarian(int userId, String passwordHash, String name, String email) {
+        super(userId, passwordHash, name, email);
+    }
 }
--- bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/team3/model/Reader.java
+++ bundle/core/src/main/java/hu/uni/miskolc/iit/swtest/team3/model/Reader.java
@@ -1,5 +1,8 @@
 package hu.uni.miskolc.iit.swtest.team3.model;
 
-public class Reader {
+public class Reader extends User{
 
+    public Reader(int userId, String passwordHash, String name, String email) {
+        super(userId, passwordHash, name, email);
+    }
 }
