8f275c6cfc432ee0129c4c94686d22af9133913b
SzaboBalint01 <szabo128@iit.uni-miskolc.hu>
2018-10-14 21:17:43 +0200

    I tried to implement the Reader service interface.

---
+++ bundle/core/service/src/smain/src/java/hu/uni/miskolc/iit/swtest/team3/interfaces/ReaderInterface.java
@@ -0,0 +1,8 @@
+package java.hu.uni.miskolc.iit.swtest.team3.interfaces;
+
+public interface ReaderInterface {
+void ListBooks();
+void RequestBook();
+void ShowBorrowings();
+void CheckAvailability();
+}
