46a6fd658232632cebc63d070a9315c5d7411b78
SzaboBalint01 <39403066+SzaboBalint01@users.noreply.github.com>
2018-10-17 22:46:52 +0200

    Delete ReaderInterface.java

--- bundle/core/service/src/smain/src/java/hu/uni/miskolc/iit/swtest/team3/interfaces/ReaderInterface.java
+++
@@ -1,8 +0,0 @@
-package java.hu.uni.miskolc.iit.swtest.team3.interfaces;
-
-public interface ReaderInterface {
-void ListBooks();
-void RequestBook();
-void ShowBorrowings();
-void CheckAvailability();
-}
