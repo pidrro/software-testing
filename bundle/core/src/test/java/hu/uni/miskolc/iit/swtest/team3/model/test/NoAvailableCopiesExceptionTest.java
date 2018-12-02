9524df1ff2a486d03bc19fbab582845411cb5f0f
SzaboBalint01 <szabo128@iit.uni-miskolc.hu>
2018-12-02 12:36:22 +0100

    Test of the NoAvailableCopies exception class.

--- bundle/core/src/test/java/hu/uni/miskolc/iit/swtest/team3/model/test/NoAvailableCopiesExceptionTest.java
+++ bundle/core/src/test/java/hu/uni/miskolc/iit/swtest/team3/model/test/NoAvailableCopiesExceptionTest.java
@@ -4,14 +4,40 @@ import hu.uni.miskolc.iit.swtest.team3.model.exception.NoAvailableCopiesExceptio
 import org.junit.Assert;
 import org.junit.Test;
 
-public class NoAvailableCopiesExceptionTest extends IllegalStateException{
+public class NoAvailableCopiesExceptionTest{
 
     private static final String MESSAGE = "Test message.";
-    private final NoAvailableCopiesException exception = new NoAvailableCopiesException(MESSAGE);
+
+    @Test
+    public NoAvailableCopiesExceptionTest(){
+        NoAvailableCopiesException exception = new NoAvailableCopiesException();
+        Assert.assertNull(exception.getMessage());
+    }
 
     @Test
     public NoAvailableCopiesExceptionTestWithMsg(){
+        NoAvailableCopiesException exception = new NoAvailableCopiesException(MESSAGE);
         Assert.assertEquals(exception.getMessage(),MESSAGE);
     }
+
+    @Test
+    public NoAvailableCopiesExceptionTestWithMsgAndCause(){
+        RuntimeException e = new RuntimeExcpetion(MESSAGE + " Tested!");
+        NoAvailableCopiesException exception = new NoAvailableCopiesException(MESSAGE, e);
+
+        Assert.assertEquals(exception.getMessage(),MESSAGE);
+        Assert.assertNotNull(exception.getCause());
+    }
+
+    @Test
+    public NoAvailableCopiesExceptionTestWithCause(){
+        RuntimeException e = new RuntimeExcpetion(MESSAGE + " Tested!");
+        NoAvailableCopiesException exception = new NoAvailableCopiesException(e);
+
+        Assert.assertNotNull(exception.getCause());
+        Assert.assertEquals(exception.getCause(),e);
+    }
+
+
 
 }
